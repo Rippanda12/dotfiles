e03c926385cf9663e43059e7155beeab99ffc7b4
Rippanda12 <59401553+Rippanda12@users.noreply.github.com>
2022-03-24 18:50:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
+sudo cp $HOME/dotfiles/etc/pacman.conf /etc/pacman.conf
+sudo cp $HOME/dotfiles/etc/makepkg.conf /etc/makepkg.conf
+sudo mkdir /repo
+sudo mount /dev/sdb3 /repo
 # software & dependencies
-sudo pacman -S git fish yay base-devel vulkan-devel wmname ranger firejail bat lsd fzf vlc  --noconfirm --needed && OK
+sudo pacman -S git fish yay base-devel vulkan-devel wmname ranger vlc  --noconfirm --needed && OK
 sudo pacman -S nodejs npm gcc rust python-pip discord --noconfirm && OK
-yay -S google-chrome spotify --noconfirm && OK
+yay -S google-chrome surfshark-vpn flameshot spotify --noconfirm && OK
 
 
 # changing shell
