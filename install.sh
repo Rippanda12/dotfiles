f6912ab90a161b5141c50c490bfac6c770e17c41
Rippanda12 <59401553+Rippanda12@users.noreply.github.com>
2022-04-03 16:06:47 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-sudo cp $HOME/dotfiles/etc/pacman.conf /etc/pacman.conf
-sudo cp $HOME/dotfiles/etc/makepkg.conf /etc/makepkg.conf
+sudo cp -v $HOME/dotfiles/etc/pacman.conf /etc/pacman.conf
+sudo cp -v $HOME/dotfiles/etc/makepkg.conf /etc/makepkg.conf
 sudo mkdir /repo
 sudo mount /dev/sdb3 /repo
+#chaotic aur
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key FBA220DFC880C036
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 # software & dependencies
+sudo pacman -Sy
 sudo pacman -S git fish yay base-devel vulkan-devel wmname ranger vlc os-prober --noconfirm --needed && OK
 sudo pacman -S nodejs npm gcc rust python-pip discord --noconfirm && OK
 sudo pacman -S mesa mesa-demos mesa-vdpau lib32-mesa lib32-libva-mesa-driver --noconfirm --needed && OK
 sudo pacman -S lib32-mea-demos lib32-mesa-vdpau lib32-opencl-mesa lib32-vulkan-mesa-layers --noconfirm --needed && OK
 sudo pacman -S libva-mesa-driver opencl-mesa vulkan-mesa-layers lib32-vulkan-radeon vulkan-radeon --noconfirm --needed && OK
-yay -S google-chrome surfshark-vpn flameshot spotify --noconfirm && OK
+yay -S google-chrome surfshark-vpn flameshot spotify fan2go --noconfirm && OK
 
 
 # changing shell
